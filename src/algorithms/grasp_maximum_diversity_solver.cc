da49a2ceb5f70f90750ae59f128773ddbddfd77d
Javier Acosta Portocarrero <alu0101660760@ull.edu.es>
2026-05-02 18:04:11 +0100

    Basic GRASP algorithm implements to build the initial solution of the B&B

---
+++ src/algorithms/grasp_maximum_diversity_solver.cc
@@ -0,0 +1,138 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Subject: Design and Analysis of Algorithms
+// Course: 3rd
+// Practice 6: Multi-Source Capacitated Facility Location Problem with Customer Incompatibilities
+// Author: Javier Acosta Portocarrero
+// Date: 04/04/2026
+// File grasp-maximum-diversity-solver.cc: implementation file.
+// Contains the implementation of the GraspMaximumDiversitySolver class.
+
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <cmath>
+
+#include "grasp_maximum_diversity_solver.h"
+#include "../solutions/maximum_diversity_solution.h"
+#include "../instances/maximum_diversity_instance.h"
+
+/**
+ * @brief Preprocesses the input instance.
+ *
+ * @param input The input instance to preprocess.
+ */
+void GraspMaximumDiversitySolver::Preprocess(Instance* input) {
+  return;
+}
+
+/**
+ * @brief Constructs a solution for the input instance.
+ *
+ * @param input The input instance for which to construct a solution.
+ * @return A pointer to the constructed solution.
+ */
+Solution* GraspMaximumDiversitySolver::ConstructSolution(Instance* input) {
+  MaximumDiversityInstance* instance = dynamic_cast<MaximumDiversityInstance*>(input);
+  if (instance == nullptr) {
+    throw std::invalid_argument("Input instance is not of type MaximumDiversityInstance.");
+  }
+  MaximumDiversitySolution* constructive_solution = new MaximumDiversitySolution(*instance, num_elements_);
+  if (constructive_solution->GetElementCount() == 0) {
+    return constructive_solution;
+  }
+
+  constructive_solution->AddElement(constructive_solution->GetInstance().GetElementsSortedByDistanceFromGlobalCenter()[0]);
+  for (size_t i = 1; i < num_elements_; ++i) {
+    std::vector<int> sorted_elements = GetSortedElementsByDistanceOfCurrentSolution(constructive_solution);
+    if (sorted_elements.size() == 0) {
+      break;
+    }
+    size_t random_element_index = rand() % std::min(lcr_cardinality_, static_cast<unsigned>(sorted_elements.size()));
+    constructive_solution->AddElement(sorted_elements[random_element_index]);
+  }
+
+  ++current_grasp_iteration_;
+  return constructive_solution;
+}
+
+/**
+ * @brief Updates the best solution found so far.
+ *
+ * @param current The current solution to compare against the best.
+ * @param best A reference to the best solution found so far.
+ */
+void GraspMaximumDiversitySolver::UpdateBest(Solution* current, Solution*& best) {
+  MaximumDiversitySolution* current_solution = dynamic_cast<MaximumDiversitySolution*>(current);
+  MaximumDiversitySolution* best_solution = dynamic_cast<MaximumDiversitySolution*>(best);
+  if (current_solution == nullptr) {
+    throw std::invalid_argument("Current solution is not of type MaximumDiversitySolution.");
+  }
+  if (best_solution == nullptr || current_solution->GetObjectiveValue() < best_solution->GetObjectiveValue()) {
+    delete best;
+    best = new MaximumDiversitySolution(*current_solution);
+  }
+}
+
+/**
+ * @brief Checks if the stopping criterion for the GRASP algorithm is met.
+ *
+ * @return true if the stopping criterion is met, false otherwise.
+ */
+bool GraspMaximumDiversitySolver::StopCriterion() {
+  if (current_grasp_iteration_ >= max_grasp_iterations_) {
+    current_grasp_iteration_ = 0;
+    return true;
+  }
+  return false;
+}
+
+/**
+ * @brief Gets the facilities sorted by their costs for a given customer.
+ *
+ * @param solution The solution for which to sort facilities.
+ * @return A vector containing the facility IDs sorted by their costs.
+ */
+std::vector<int> GraspMaximumDiversitySolver::GetSortedElementsByDistanceOfCurrentSolution(MaximumDiversitySolution* solution) const {
+  std::vector<std::pair<int, double>> elements_with_distance;
+  std::vector<double> contribution_by_element = solution->GetContributionByElement();
+  for (int element_id = 0; element_id < solution->GetElementCount(); ++element_id) {
+    if (solution->IsElementSelected(element_id)) {
+      continue;
+    }
+    elements_with_distance.emplace_back(element_id, contribution_by_element[element_id]);
+  }
+  std::sort(elements_with_distance.begin(), elements_with_distance.end(),
+            [](const std::pair<int, double>& a, const std::pair<int, double>& b) {
+              return a.second < b.second;
+            });
+  std::vector<int> elements_by_distance;
+  for (const auto& pair : elements_with_distance) {
+    elements_by_distance.push_back(pair.first);
+  }
+
+  return elements_by_distance;
+}
+
+/**
+ * @brief Postprocesses a solution after construction using a VND strategy.
+ *
+ * @param solution The solution to postprocess.
+ * @return A pointer to the postprocessed solution.
+ */
+Solution* GraspMaximumDiversitySolver::Postprocess(Solution* solution) {
+  MaximumDiversitySolution* current_solution = dynamic_cast<MaximumDiversitySolution*>(solution);
+  if (current_solution == nullptr) {
+    throw std::invalid_argument("Current solution is not of type MaximumDiversitySolution.");
+  }
+  for (const auto& explorer : neighboorhod_explorers_) {
+    bool improvement = true;
+    while (improvement) {
+      improvement = explorer->Explore(current_solution, GetAmountTolerance(), GetImprovementTolerance());
+    }
+  }
+  return current_solution;
+}
---
+++ src/algorithms/grasp_maximum_diversity_solver.h
@@ -0,0 +1,52 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Subject: Design and Analysis of Algorithms
+// Course: 3rd
+// Practice 6: Multi-Source Capacitated Facility Location Problem with Customer Incompatibilities
+// Author: Javier Acosta Portocarrero
+// Date: 04/04/2026
+// File grasp-maximum-diversity-solver.h: declaration file.
+// Contains the declaration of the GraspMaximumDiversitySolver class.
+
+#ifndef GRAASP_MAXIMUM_DIVERSITY_SOLVER_H_
+#define GRAASP_MAXIMUM_DIVERSITY_SOLVER_H_
+
+#include "grasp_algorithm.h"
+#include "../instances/maximum_diversity_instance.h"
+#include "../solutions/maximum_diversity_solution.h"
+#include "../explorers/maximum_diversity_neighborhood_explorer.h"
+
+/**
+ * @class GraspMaximumDiversitySolver
+ *
+ * @brief Class that implements the GRASP metaheuristic to solve the Maximum Diversity problem.
+ */
+class GraspMaximumDiversitySolver : public GraspAlgorithm {
+ public:
+  GraspMaximumDiversitySolver(unsigned cardinality = 1, std::vector<MaximumDiversityNeighborhoodExplorer*> explorers = {}, unsigned num_elements = 1) : 
+                      lcr_cardinality_{cardinality}, neighboorhod_explorers_{explorers}, num_elements_{num_elements} {}
+  ~GraspMaximumDiversitySolver() override = default;
+ protected:
+  void Preprocess(Instance* input) override;
+  Solution* ConstructSolution(Instance* input) override;
+  Solution* Postprocess(Solution* solution) override;
+  void UpdateBest(Solution* current, Solution*& best) override;
+  bool StopCriterion() override;
+  const std::vector<MaximumDiversityNeighborhoodExplorer*>& GetNeighborhoodExplorers() const { return neighboorhod_explorers_;}
+  double GetAmountTolerance() const { return kAmountTolerance_;}
+  double GetImprovementTolerance() const { return kImprovementTolerance_;}
+  
+ private:
+  unsigned current_grasp_iteration_ = 0;
+  unsigned max_grasp_iterations_ = 1;
+  unsigned lcr_cardinality_ = 1;
+  unsigned num_elements_ = 1;
+  std::vector<MaximumDiversityNeighborhoodExplorer*> neighboorhod_explorers_;
+  static constexpr double kAmountTolerance_ = 1e-8;
+  static constexpr double kImprovementTolerance_ = 1e-8;
+
+  std::vector<int> GetSortedElementsByDistanceOfCurrentSolution( MaximumDiversitySolution* solution) const;
+};
+
+#endif
